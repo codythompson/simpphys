da2c3a55be4239cd96bb4d5c28a39a916b13025e
Cody Thompson <cody.thompson22@gmail.com>
2018-01-27 23:43:33 -0800

    removed test test ;)

--- test/HelloWorld.js
+++
@@ -1,8 +0,0 @@
-const should = require('chai').should();
-
-describe('HelloWorld', function () {
-  const helloWorld = 'ello luv';
-  it ('should equal "ello luv"', function () {
-    helloWorld.should.equal('ello luv');
-  });
-});
